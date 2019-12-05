1c042a195893a15102540c8913a17ca932f5daad
beapen <github@gulfdoctor.net>
2019-12-05 07:38:17 -0500

    progress: dad_env action and observation space

--- dad_gym_env/envs/dad_env.py
+++ dad_gym_env/envs/dad_env.py
@@ -11,6 +11,22 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 class DadEnv(gym.Env):
     metadata = {'render.modes': ['human']}
+    
+
+    def __init__(self, treatment_no):
+        super(DadEnv, self).__init__()
+        self.NUMBER_FACTORS = 2000
+        # Action spaces are discrete with # = n^2 interventions considered
+        if treatment_no ^ 2 < 1:
+            discrete_actions = 2
+        else:
+            discrete_actions = treatment_no ^ 2
+        self.action_space = spaces.Discrete(discrete_actions)
+        # Observation space is a vector of length 2000
+        self.observation_space = spaces.Box(
+                                    low=0, high=1, 
+                                    shape=(1,self.NUMBER_FACTORS), 
+                                            dtype=np.uint8)
 
     def step(self, action):
         pass
